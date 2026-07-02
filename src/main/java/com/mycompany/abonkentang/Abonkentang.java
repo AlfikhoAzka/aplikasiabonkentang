0b0a1333a96f628a85f116e1c4f9ca900ace41e9
Alfikho Azka <alfikhoazka33@gmail.com>
2026-07-02 14:07:55 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/abonkentang/Abonkentang.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.abonkentang;
+
+/**
+ *
+ * @author Alfikho Azka
+ */
+public class Abonkentang {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

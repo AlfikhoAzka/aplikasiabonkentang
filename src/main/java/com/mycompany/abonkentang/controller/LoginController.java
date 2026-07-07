7ec7146c91786612b8baaf1f495e03e0cd49757a
Bintang Aziz <Bintang Aziz@Bintang>
2026-07-07 18:56:23 +0700

    commit

--- src/main/java/com/mycompany/abonkentang/controller/LoginController.java
+++ src/main/java/com/mycompany/abonkentang/controller/LoginController.java
@@ -4,10 +4,43 @@
  */
 package com.mycompany.abonkentang.controller;
 
+import com.mycompany.abonkentang.config.koneksi;
+import com.mycompany.abonkentang.model.User;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 /**
  *
  * @author Kelompok 6
  */
 public class LoginController {
-    
+
+    public User login(String username, String password) {
+        User user = null;
+        try {
+            Connection conn = koneksi.getKoneksi();
+
+            String sql = "SELECT * FROM user WHERE username = ? AND password = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                user = new User(
+                    rs.getInt("id_user"),
+                    rs.getString("username"),
+                    rs.getString("password"),
+                    rs.getString("nama_lengkap"),
+                    rs.getString("role")
+                );
+            }
+
+        } catch (Exception e) {
+            System.out.println("Gagal login: " + e.getMessage());
+        }
+        return user;
+    }
 }
--- src/main/java/com/mycompany/abonkentang/model/User.java
+++ src/main/java/com/mycompany/abonkentang/model/User.java
@@ -6,8 +6,26 @@ package com.mycompany.abonkentang.model;
 
 /**
  *
- * @author Alfikho Azka
+ * @author Kelompok 6
  */
 public class User {
-    
+    private int idUser;
+    private String username;
+    private String password;
+    private String namaLengkap;
+    private String role;
+
+    public User(int idUser, String username, String password, String namaLengkap, String role) {
+        this.idUser = idUser;
+        this.username = username;
+        this.password = password;
+        this.namaLengkap = namaLengkap;
+        this.role = role;
+    }
+
+    public int getIdUser() { return idUser; }
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public String getNamaLengkap() { return namaLengkap; }
+    public String getRole() { return role; }
 }

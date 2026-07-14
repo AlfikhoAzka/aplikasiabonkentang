5ddb9feda85d2d30c1dab03006c00bcf82f5195d
Alfikho Azka <alfikhoazka33@gmail.com>
2026-07-14 14:56:02 +0700

    commit berisi laporan produksi

---
+++ src/main/java/com/mycompany/abonkentang/model/LaporanProduksi.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.abonkentang.model;
+import java.util.Date;
+
+/**
+ *
+ * @author Kelompok 6
+ */
+public class LaporanProduksi {
+
+    private Date tanggalProduksi;
+    private String namaProduk;
+    private int jumlahProduksi;
+    private String keterangan;
+
+    public Date getTanggalProduksi() {
+        return tanggalProduksi;
+    }
+
+    public void setTanggalProduksi(Date tanggalProduksi) {
+        this.tanggalProduksi = tanggalProduksi;
+    }
+
+    public String getNamaProduk() {
+        return namaProduk;
+    }
+
+    public void setNamaProduk(String namaProduk) {
+        this.namaProduk = namaProduk;
+    }
+
+    public int getJumlahProduksi() {
+        return jumlahProduksi;
+    }
+
+    public void setJumlahProduksi(int jumlahProduksi) {
+        this.jumlahProduksi = jumlahProduksi;
+    }
+
+    public String getKeterangan() {
+        return keterangan;
+    }
+
+    public void setKeterangan(String keterangan) {
+        this.keterangan = keterangan;
+    }
+}
